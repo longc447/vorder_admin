dc47317950821d5eb29adca9b5873de3a0aa0814
344403155@qq.com <344403155@qq.com>
2021-04-11 03:13:53 +0800

    reactive,ref,生命周期

--- src/views/menu/Menu.vue
+++ src/views/menu/Menu.vue
@@ -1,54 +1,96 @@
 
 <template>
-  <div>sss</div>
-  <el-table :data="tableData" style="width: 100%">
-    <el-table-column prop="date" label="日期" width="180"> </el-table-column>
-    <el-table-column prop="name" label="姓名" width="180"> </el-table-column>
-    <el-table-column prop="address" label="地址"> </el-table-column>
-  </el-table>
+  <div class="menuBox">
+    <div class="buttonBox">
+      <el-button
+        type="primary"
+        size="small"
+        icon="el-icon-plus"
+        @click="menuDialog.handle"
+        >添加菜品</el-button
+      >
+    </div>
+    <el-table :data="tableData.data" class="table" border>
+      <el-table-column prop="_id" label="id" width="180"> </el-table-column>
+      <el-table-column prop="name" label="姓名" width="180"> </el-table-column>
+      <el-table-column prop="pic" label="图片"> </el-table-column>
+    </el-table>
+  </div>
+
+  <el-dialog
+    title="提示"
+    v-model="menuDialog.isShow"
+    width="30%"
+    :before-close="menuDialog.close"
+  >
+    <span>这是一段信息</span>
+    <template #footer>
+      <span class="dialog-footer">
+        <el-button @click="menuDialog.isShow = false">取 消</el-button>
+        <el-button type="primary" @click="menuDialog.isShow = false"
+          >确 定</el-button
+        >
+      </span>
+    </template>
+  </el-dialog>
+
+  <div class="menuform" v-if="menuShow">添加菜单</div>
 </template>
 
-<script>
+<script lang="typescript">
+import { onMounted, reactive, ref } from "vue";
+import http from "@/config/telnet/http";
+import api from "@/config/telnet/api";
 export default {
   data() {
+    return {};
+  },
+  setup() {
+    //表格
+    let tableData = reactive({
+      data: [],
+    });
+    async function getDate() {
+      let a = await http.get(api.get("menu"));
+      tableData.data = a.data;
+    }
+    //弹出框
+    let menuDialog = reactive({
+      isShow: false,
+      handle: () => {
+        menuDialog.isShow = !menuDialog.isShow;
+      },
+      close: () => {
+        
+      },
+    });
+
+    onMounted(() => {
+      getDate();
+    });
+    
     return {
-      tableData: [
-        {
-          date: "2016-05-02",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1518 弄",
-        },
-        {
-          date: "2016-05-04",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1517 弄",
-        },
-        {
-          date: "2016-05-01",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1519 弄",
-        },
-        {
-          date: "2016-05-03",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1516 弄",
-        },
-      ],
+      tableData,
+      menuDialog,
     };
-  },
-  mounted() {
-      this.getDate()
-  },
-  methods: {
-    async getDate() {
-      let url = this.$Api.get("menu");
-      console.log(url);
-      let i =await this.$Http.get(url);
-      console.log(i);
-    },
   },
 };
 </script>
 
-<style>
+<style lang="scss" scope>
+.menuBox {
+  padding: 10px;
+  .buttonBox {
+    margin-bottom: 10px;
+  }
+  .table {
+    margin: 0 auto;
+  }
+}
+
+.menuform {
+  width: 200px;
+  height: 200px;
+  border: 1xp solid #666666;
+}
 </style>
